da7282866821298c30bb16a3605a5da1b3594194
Matan Post <matanpost@gmail.com>
2023-02-23 21:55:49 +0100

    iteration 2.4 still not done

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,38 +1,23 @@
 import React from 'react'
 import { useEffect } from 'react'
-import {useParams} from 'react-router-dom'
+import { createSearchParams, useParams, useSearchParams } from 'react-router-dom'
 
 function CountryDetails(countries) {
-    const { alpha3Code } = useParams()
-    const foundCountry = countries.find((oneCountry) => {
-        return oneCountry.alpha3Code === alpha3Code;
-      });
+     const { alpha3Code } = useParams()
+    const [searchParams, setSearchParams] = useSearchParams({alpha3Code}) 
 
-      console.log(foundCountry.area)
-
+    useEffect(() => {
+        searchParams.get('alpha3Code')
+       
+      }, [searchParams])   
+    console.log(`${alpha3Code}`);
     return (
-   
-  
-      <div>
-          <h2>{foundCountry.name.official}</h2>
-          <table class="table">
-            <tbody>
-              <tr>
-                <th scope="row">Capital</th>
-                <td>{foundCountry.capital}</td>
-              </tr>
-              <tr>
-                <th scope="row">Area</th>
-                <td>{foundCountry.area} km²</td>
-              </tr>
-              
-            </tbody>
-          </table>
-        </div>
-
-    
-        )
+    <div>
+       
+    </div>
+  )
 }
+
 
 
 export default CountryDetails
